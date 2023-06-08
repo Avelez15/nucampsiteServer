e3cee2fa9a5dde929d81bf3a98864041a226be32
Alejandro Velez <velez.alejandro4@gmail.com>
2023-06-08 19:57:48 -0400

    Express REST API part 1

---
+++ models/campsite.js
@@ -0,0 +1,64 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+require('mongoose-currency').loadType(mongoose);
+const Currency = mongoose.Types.Currency;
+
+
+
+const commentSchema = new Schema({
+    rating: {
+        type: Number,
+        min: 1,
+        max: 5,
+        required: true
+    },
+    text: {
+        type: String,
+        required: true
+    },
+    author: {
+        type: String,
+        required: true
+    }
+}, {
+    timestamps: true
+});
+
+//below is used to create a schema, creating configurations for the doc.
+const campsiteSchema = new Schema({
+    name: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    description: {
+        type: String,
+        required: true
+    },
+    image: {
+        type: String,
+        required: true
+    },
+    elevation: {
+        type: Number,
+        required: true
+    },
+    cost: {
+        type: Currency,
+        required: true,
+        min: 0
+    },
+    featured: {
+        type: Boolean,
+        default: false
+    },
+    comments: [commentSchema]
+}, {
+    timestamps: true
+});
+//second optional argument is used to set configuration options
+//below creates a model, const must be capitalized and name string must be the same, second argument must be schema you wish to use.
+
+const Campsite = mongoose.model('Campsite', campsiteSchema);
+
+module.exports = Campsite;
