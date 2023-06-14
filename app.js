f7fb859ec01faab114152c24de1ef40b80b6e60a
Alejandro Velez <velez.alejandro4@gmail.com>
2023-06-14 19:31:58 -0400

    Express session 2

--- app.js
+++ app.js
@@ -46,35 +46,19 @@ app.use(session({
   resave: false,
   store: new fileStore()
 }));
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
 
 function auth(req, res, next) {
   console.log(req.session);
   //signedCookies is provided by cookieParser and will automatically parse a signed cookie from the request, if cookie not properly signed, returns a property of false.
   if (!req.session.user) {
-    const authHeader = req.headers.authorization;
-    if (!authHeader) {
-      const err = new Error('You are not authenticated!');
-      res.setHeader('WWW-Authenticate', 'Basic');
-      err.status = 401;
-      return next(err);
-    }
-
-    const auth = Buffer.from(authHeader.split(' ')[1], 'base64').toString().split(':');
-    const user = auth[0];
-    const pass = auth[1];
-    if (user === 'admin' && pass === 'password') {
-      //res.cookie is part of expresses response API, this uses it to create a new cookie 
-      req.session.user = 'admin';
-      return next();//authorized
-    } else {
-      const err = new Error('You are not authorized!');
-      res.setHeader('WWW-Authenticate', 'Basic');
-      err.status = 401;
-      return next(err);
-    }
+    const err = new Error('You are not authenticated!');
+    err.status = 401;
+    return next(err);
     //below will mean if there is a signedCookies.user value.
   } else {
-    if (req.session.user === 'admin') {
+    if (req.session.user === 'authenticated') {
       return next();
     } else {
       const err = new Error('You are not authorized!');
@@ -88,8 +72,7 @@ app.use(auth);
 
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+
 app.use('/campsites', campsiteRouter);
 app.use('/promotions', promotionRouter);
 app.use('/partners', partnerRouter);
