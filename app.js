c364e1599b8cfba80cf5ae53e1089426c1d40f10
Alejandro Velez <velez.alejandro4@gmail.com>
2023-06-24 11:09:29 -0400

    workshop 4

--- app.js
+++ app.js
@@ -11,9 +11,9 @@ const campsiteRouter = require('./routes/campsiteRouter');
 const promotionRouter = require('./routes/promotionRouter');
 const partnerRouter = require('./routes/partnerRouter');
 const uploadRouter = require('./routes/uploadRouter');
+const favoriteRouter = require('./routes/favoriteRouter');
 
 const mongoose = require('mongoose');
-const { start } = require('repl');
 
 const url = config.mongoUrl;
 const connect = mongoose.connect(url, {
@@ -28,8 +28,8 @@ connect.then(() => console.log('Connected correctly to server'),
 );
 
 var app = express();
-//app.all below catches all requests that come into the server
-//secure traffic only
+
+
 app.all('*', (req, res, next) => {
   if (req.secure) {
     return next();
@@ -39,48 +39,39 @@ app.all('*', (req, res, next) => {
   }
 });
 
-// view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-//app.use(cookieParser('12345-67890-09870-54321'));
 
 
 app.use(passport.initialize());
-
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
 app.use(express.static(path.join(__dirname, 'public')));
 
-
 app.use('/campsites', campsiteRouter);
 app.use('/promotions', promotionRouter);
 app.use('/partners', partnerRouter);
 app.use('/imageUpload', uploadRouter);
+app.use('/favorites', favoriteRouter);
 
-// catch 404 and forward to error handler
+
 app.use(function (req, res, next) {
   next(createError(404));
 });
 
-// error handler
+
 app.use(function (err, req, res, next) {
-  // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
   res.status(err.status || 500);
   res.render('error');
 });
 
 module.exports = app;
-
-//                                        Notes
-  //when new session is created, but no new updates are made to it, then at the end of the request it won't get saved since it will just be an empty session.
-  //signedCookies is provided by cookieParser and will automatically parse a signed cookie from the request, if cookie not properly signed, returns a property of false.
